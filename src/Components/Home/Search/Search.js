725089c4379fcd2bc4e24d7dad1d272934ecc595
Mdkamrulislam786 <kamrulislam.ki01@gmail.com>
2020-08-30 19:59:57 +0600

    adding-react-calender

--- src/Components/Home/Search/Search.js
+++ src/Components/Home/Search/Search.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Search.css";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 
 const Search = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
   return (
     <div>
-      <h2>Hello from Search</h2>
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+      <h2>Number of Guests</h2>
     </div>
   );
 };
 
-export default Search;
+export default Search;      
